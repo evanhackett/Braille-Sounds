11039ddebd87e00a7394e7c0ed61e05c400712b0
evanhackett <evanwhackett@gmail.com>
2018-07-27 16:11:33 -0700

    Now that six-key-input exposes onStart handlers, this has been refactored to a cleaner design. All note combos now play.

--- App.js
+++ App.js
@@ -8,36 +8,26 @@ import { Audio } from 'expo';
 export default class App extends React.Component {
 
   state = {
-    'a1': null,
-    'b1': null,
-    'c1': null,
-    'd1': null,
-    'e1': null,
-    'f1': null,
+    soundObjects: []
   }
 
   async loadSounds() {
-    const soundObjectA1 = new Expo.Audio.Sound();
-    const soundObjectB1 = new Expo.Audio.Sound();
     const soundObjectC1 = new Expo.Audio.Sound();
     const soundObjectD1 = new Expo.Audio.Sound();
     const soundObjectE1 = new Expo.Audio.Sound();
     const soundObjectF1 = new Expo.Audio.Sound();
+    const soundObjectG1 = new Expo.Audio.Sound();
+    const soundObjectA1 = new Expo.Audio.Sound();
     try {
-      await soundObjectA1.loadAsync(require('./assets/sounds/a1.mp3'));
-      await soundObjectB1.loadAsync(require('./assets/sounds/b1.mp3'));
       await soundObjectC1.loadAsync(require('./assets/sounds/c1.mp3'));
       await soundObjectD1.loadAsync(require('./assets/sounds/d1.mp3'));
       await soundObjectE1.loadAsync(require('./assets/sounds/e1.mp3'));
       await soundObjectF1.loadAsync(require('./assets/sounds/f1.mp3'));
+      await soundObjectG1.loadAsync(require('./assets/sounds/g1.mp3'));
+      await soundObjectA1.loadAsync(require('./assets/sounds/a1.mp3'));
 
       this.setState({
-        'a1': soundObjectA1,
-        'b1': soundObjectB1,
-        'c1': soundObjectC1,
-        'd1': soundObjectD1,
-        'e1': soundObjectE1,
-        'f1': soundObjectF1,
+        soundObjects: [soundObjectC1, soundObjectD1, soundObjectE1, soundObjectF1, soundObjectG1, soundObjectA1]
       })
     } catch (error) {
       // An error occurred!
@@ -51,59 +41,23 @@ export default class App extends React.Component {
     })
   }
 
-  _onChangeHandler = value => {
-    console.log(value);
-
-    switch (value) {
-      case '⠁':
-        this.state.a1.replayAsync().then(() => {console.log('Playing note: a1');})
-        break;
-      case '⠂':
-        this.state.b1.replayAsync().then(() => {console.log('Playing note: b1');})
-        break;
-      case '⠄':
-        this.state.c1.replayAsync().then(() => {console.log('Playing note: c1');})
-        break;
-      case '⠈':
-        this.state.d1.replayAsync().then(() => {console.log('Playing note: d1');})
-        break;
-      case '⠐':
-        this.state.e1.replayAsync().then(() => {console.log('Playing note: e1');})
-        break;
-      case '⠠':
-        this.state.f1.replayAsync().then(() => {console.log('Playing note: f1');})
-        break;
-      case '⠃':
-        this.state.a1.replayAsync().then(() => {console.log('Playing note: a1');})
-        this.state.b1.replayAsync().then(() => {console.log('Playing note: b1');})
-        break;
-      case '⠇':
-      case '⠿':
-        this.state.a1.replayAsync().then(() => {console.log('Playing note: a1');})
-        this.state.b1.replayAsync().then(() => {console.log('Playing note: b1');})
-        this.state.c1.replayAsync().then(() => {console.log('Playing note: c1');})
-        this.state.d1.replayAsync().then(() => {console.log('Playing note: d1');})
-        this.state.e1.replayAsync().then(() => {console.log('Playing note: e1');})
-        this.state.f1.replayAsync().then(() => {console.log('Playing note: f1');})
-        break;
-      default:
-        console.log('Unhandled case!');
-    }
+  createOnTouchStartFn = i => () => {
+    this.state.soundObjects[i-1].replayAsync().then(() => {console.log('Pressed', i);})
   }
 
-  _clearResultValue = () => {
-  }
-
-  _onDelete = () => {
+  createOnTouchEndFn = i => () => {
+    console.log('released', i);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <SixKeyInput
-          onChange={this._onChangeHandler}
-          onDelete={this._onDelete}
-          clear={this._clearResultValue}
+          onChange={() => {}}
+          onDelete={() => {}}
+          clear={() => {}}
+          onTouchStartFunctions={[1,2,3,4,5,6].map(this.createOnTouchStartFn)}
+          onTouchEndFunctions={[1,2,3,4,5,6].map(this.createOnTouchEndFn)}
         />
       </View>
     );
